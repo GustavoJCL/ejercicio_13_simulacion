dc973fe8c15c3c6d168078e9118100aa7f0107ef
GustavoJCL <gjcalizayal@unjbg.edu.com>
2023-10-04 19:23:25 -0500

    implementacion de componentes en svelte

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+
+	let selectedOption = '';
+
+	const handleOptionSelect = (event: Event) => {
+		const select = event.target as HTMLSelectElement;
+		selectedOption = select.value;
+		// Handle the selected option here
+		console.log(selectedOption);
+	};
+	let currentTitle = 0;
+</script>
+
+<div>
+	<AppRail>
+		<svelte:fragment slot="lead">
+			<AppRailAnchor href="/">(icon)</AppRailAnchor>
+		</svelte:fragment>
+		<AppRailTile bind:group={currentTitle} name="title-1" value="0" title="title-2">
+			<AppRailAnchor href="#" on:click|preventDefault={handleOptionSelect}>
+				Select an option
+			</AppRailAnchor>
+		</AppRailTile>
+		<AppRailTile>
+			<AppRailAnchor
+				href="/prueba_huecos"
+				on:click|preventDefault={handleOptionSelect}
+				value="Prueba de Huecos"
+			>
+				Prueba de Huecos
+			</AppRailAnchor>
+		</AppRailTile>
+		<AppRailTile>
+			<AppRailAnchor href="#" on:click|preventDefault={handleOptionSelect} value="Prueba de Pocker">
+				Prueba de Pocker
+			</AppRailAnchor>
+		</AppRailTile>
+		<AppRailTile>
+			<AppRailAnchor
+				href="#"
+				on:click|preventDefault={handleOptionSelect}
+				value="Prueba arriba abajo"
+			>
+				Prueba arriba abajo
+			</AppRailAnchor>
+		</AppRailTile>
+		<AppRailTile>
+			<AppRailAnchor
+				href="#"
+				on:click|preventDefault={handleOptionSelect}
+				value="Prueba arriba abajo de medias"
+			>
+				Prueba arriba abajo de medias
+			</AppRailAnchor>
+		</AppRailTile>
+		<AppRailTile>
+			<AppRailAnchor href="#" on:click|preventDefault={handleOptionSelect} value="Prueba de Series">
+				Prueba de Series
+			</AppRailAnchor>
+		</AppRailTile>
+	</AppRail>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
+	import Menu from '$lib/Menu.svelte';
 	import Greet from '../lib/Greet.svelte';
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+	let files: File[] = [];
+	function handleDrop(event: any) {
+		files = event.detail.files;
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -7,3 +13,8 @@
 
 <h1>Welcome to SvelteKit</h1>
 <Greet />
+
+<div>
+	<FileDropzone name="file_upload" on:drop={handleDrop} />
+	<Menu />
+</div>
